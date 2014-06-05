d9b4ced12478b70e7d06ef99350c57a63690e4aa
Juan Edi <jedi@manas.com.ar>
2014-06-05 16:32:23 -0300

    Secure cookies when using HTTPS

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Poirot::Application.config.session_store :cookie_store, key: '_poirot_session'
+Poirot::Application.config.session_store :cookie_store, { key: '_poirot_session', secure: Settings.protocol.eql?("https") }
